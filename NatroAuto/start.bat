10d43c1808b26c5af49f5c3a12dc3d01cef6f255
Shawn <public@somerandomplayer.dev>
2024-03-05 16:37:25 +0100

    Update start.bat
    made the bat work with the new update

--- NatroAuto/start.bat
+++ NatroAuto/start.bat
@@ -1,4 +1,8 @@
 @echo off
+echo NatroAuto
+echo Discord bot by SRP
+echo Natro Macro made by the Natro Team
+echo The bot will be minimized on taskbar.
 echo installing requirements...
-pip install nextcord pyautogui setuptools pywin32 configparser pillow
-python bot.py
+pip install nextcord pyautogui setuptools pywin32 configparser pillow pystray
+start pythonw.exe bot.py
